05ddc9cea9785631a761e2b7aa4811d08747e569
wansong <betterofsong@gmail.com>
2021-04-24 16:14:04 +0800

    selfsign.sh

---
+++ selfsign.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -eu
+
+show_help() {
+  echo "$0 --key <keyfile> --out <outfile>"
+}
+
+keyfile=
+outfile=
+days=365
+while [ "$#" -gt 0 ] ; do
+  case "$1" in
+    --key)
+      keyfile="$2"
+      shift ; shift
+      ;;
+    --out)
+      keyfile="$2"
+      shift ; shift
+      ;;
+    --days)
+      days="$2"
+      shift ; shift
+      ;;
+    *)
+      show_help
+      exit
+      shift
+      ;;
+  esac
+done
+
+if ! [ -f "$keyfile" ]  ; then
+  show_help
+  exit 1
+fi
+
+if [ -z "$outfile" ] ; then
+  outfile="${keyfile}.ca.crt.pem"
+fi
+
+openssl req -new -x509 -days "$days" -key "$keyfile"  -out "$outfile"
