c385119dbc5b2b3e0eb0dcd11df7c22e02f7d598
wansong <betterofsong@gmail.com>
2021-04-24 16:28:36 +0800

    makep12.sh

---
+++ makep12.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -eu
+
+show_help() {
+  echo "$0 --cert <cert> --key <keyfile> --out <outfile>"
+}
+
+certfile=
+keyfile=
+outfile=
+while [ "$#" -gt 0 ] ; do
+  case "$1" in
+    --cert)
+      certfile=$2
+      shift ; shift
+      ;;
+    --key)
+      keyfile=$2
+      shift ; shift
+      ;;
+    --out)
+      outfile=$2
+      shift ; shift
+      ;;
+    *)
+      show_help
+      exit 1
+      shift
+      ;;
+  esac
+done
+
+if [ -z "$certfile" ] || [ -z "$keyfile" ] ; then
+  show_help
+  exit 1
+fi
+
+if [ -z "$outfile" ] ; then
+  outfile="${certfile}.p12"
+fi
+
+openssl pkcs12 -export -in "$certfile"  -inkey "$keyfile" -out "$outfile"
