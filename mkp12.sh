9c945c312a9503b28934acec59f9fd2e2dbc5ef2
wansong <betterofsong@gmail.com>
2023-02-23 21:20:16 +0800

    update mkp12.sh

--- mkp12.sh
+++ mkp12.sh
@@ -5,6 +5,7 @@ show_help() {
   echo "$0 --cert <cert> --key <keyfile> --out <outfile>"
 }
 
+relay_args=()
 certfile=
 keyfile=
 outfile=
@@ -23,8 +24,7 @@ while [ "$#" -gt 0 ] ; do
       shift ; shift
       ;;
     *)
-      show_help
-      exit 1
+      relay_args+=("$1")
       shift
       ;;
   esac
@@ -39,4 +39,4 @@ if [ -z "$outfile" ] ; then
   outfile="${certfile}.p12"
 fi
 
-openssl pkcs12 -export -in "$certfile"  -inkey "$keyfile" -out "$outfile"
+openssl pkcs12 -export -in "$certfile"  -inkey "$keyfile" -out "$outfile" "${relay_args[@]}"
